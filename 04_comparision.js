4616b7df67074a5b2e108207459c5ba221e186ba
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-08-27 05:33:12 +0000

    Day2(part1)

---
+++ 04_comparision.js
@@ -0,0 +1,32 @@
+console.log(2 > 1);
+console.log(2 != 1);
+
+//problem ati he jab ap same datatype compare nahi karte he 
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+
+console.log( null == 0);
+console.log(null >= 0);  //iska output 1 he jab ki == ka output false he kyuki intially > will 
+//convert null to 0 and then we equate it will be true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+//strict check === it checks value and datatype
+
+
+
+
+
+
+
+
+
+
+
+
