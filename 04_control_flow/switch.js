62a0b49d48926d155bf811f73ef5c4d7f1d72554
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-09-06 09:02:38 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3  //agar yeh string hota he "3" toh yeh fail hoga kyuki switch me no dala he string dal pass hoga
+
+
+
+switch (month) {
+    case 1:
+        console.log("its jan");
+        
+        break;
+    case 2:
+        console.log("its feb");
+        
+        break;
+    case 3:
+        console.log("its march");
+        
+        break;
+    case 4:
+        console.log("its april");
+        
+        break;
+
+    default:
+        console.log("defualt case");
+        
+        break;
+}
