5dccc1c50d88932e01e07b0ae01a3dcbc6488f2a
mr-raj2001 <www.rishav2001@gmail.com>
2024-09-08 19:28:15 +0530

    for loop

---
+++ 05_itterations/one.js
@@ -0,0 +1,78 @@
+//for
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(
+//         element == 5
+//     ){
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(element);
+    
+    
+// }
+
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+
+//     console.log(`Outer loop value is ${index}`);
+    
+
+//     for (let z = 0; z <= 10; z++) {
+//         const element = z;
+
+//         console.log(`Inner loop value is ${index} and ${z}`);
+        
+        
+//     }
+    
+// }
+
+
+// let myArray = ["flash", "batman", "superan"]
+
+// console.log(myArray.length);
+
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+//shift+alt+up/down arrow key
+//ctrl+d
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(index);
+//         break;
+//     }
+
+
+//     console.log(index);
+    
+    
+    
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log('skipped 5');
+        continue;
+    }
+
+
+    console.log(index);
+    
+    
+    
+    
+}
+
