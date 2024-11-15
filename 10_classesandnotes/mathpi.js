88e03ce92be30f5a3e4489b7491e1ca9c2a40c74
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-11-15 09:14:52 +0000

    mathpi

---
+++ 10_classesandnotes/mathpi.js
@@ -0,0 +1,8 @@
+//hame pata he object ka direct access he hamare pass
+
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+console.log(descripter);
+
+
+//define property to set writable emaunarable 
