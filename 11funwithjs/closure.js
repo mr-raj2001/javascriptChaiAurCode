724a31b574a68a03eb3df3e44b61221fdf21d972
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-11-19 06:47:20 +0000

    lexical or colosure

---
+++ 11funwithjs/closure.js
@@ -0,0 +1,10 @@
+//closure example documentation mdn 
+//lexical scoping allows inner function to have access to scope of outer function
+
+//ab bas parent se le sakte he bhai bhai cant share inner cant share 
+
+//and parent bhi inner ka access nahi kar sakte he 
+
+//closure me pura lexical scope pura jata he sirf context nahi isiliye woh run se hath bhi jaye inner function par milega usko tab bhi 
+
+//closure color exmaple  in video 
